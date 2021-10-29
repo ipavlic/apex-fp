cc6cb5c9e3a6d6c6089f3978fef321493c5197ca
Ilija Pavlic <ilija.pavlic@gmail.com>
2021-10-29 06:24:57 -0400

    Temporary disable contextual search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -120,7 +120,7 @@ const config = {
         appId: 'BH4D9OD16A',
         apiKey: '74336c94033d588be178a3757c5bc716',
         indexName: 'apexfp',
-        contextualSearch: true
+        contextualSearch: false
       },
       gtag: {
         trackingID: 'G-PNTCNPSG2Z',
